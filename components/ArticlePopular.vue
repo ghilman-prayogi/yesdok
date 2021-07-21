e39b28bade4c71c52373e9ad840c1ea4d8d8cdf2
Ghilman <ghilman.prayogi@cartenz.co.id>
2021-07-21 13:22:23 +0700

    Responsive fix

--- components/ArticlePopular.vue
+++ components/ArticlePopular.vue
@@ -5,7 +5,7 @@
             <div class="md:flex-shrink-0">
                 <img class="h-20 w-full object-cover md:w-20" :src="list.urlToImage">
             </div>
-            <div class="p-4 pt-0 pb-0">
+            <div class="py-2 md:px-4">
                 <a :href="list.url" class="line-clamp-3 block text-sm leading-tight font-medium text-black hover:underline">{{ list.title }}</a>
                 <div class="flex inline-flex text-xs text-gray-400 mt-2">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
